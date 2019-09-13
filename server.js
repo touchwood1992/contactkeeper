7f787b5d9eab049e1820ef2008b4d6f91493e691
test cloud <test.cloudweblabs@gmail.com>
2019-09-13 12:45:31 +0530

    Ready for awesome2

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 const dbCommection = require('./config/db');
 dbCommection();
 app.use(express.json({ extended: false }));
@@ -7,6 +10,12 @@ app.use(express.json({ extended: false }));
 app.use('/api/user', require('./route/api/user'));
 app.use('/api/auth', require('./route/api/auth'));
 app.use('/api/contact', require('./route/api/contact'));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running on ports ${PORT}`));
