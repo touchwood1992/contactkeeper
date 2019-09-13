ad5c6e3f53c47cb7e308ee2b7ab24d29e3966929
test cloud <test.cloudweblabs@gmail.com>
2019-09-13 12:53:02 +0530

    Ready for awesome3

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 const dbCommection = require('./config/db');
 dbCommection();
@@ -11,11 +10,13 @@ app.use('/api/user', require('./route/api/user'));
 app.use('/api/auth', require('./route/api/auth'));
 app.use('/api/contact', require('./route/api/contact'));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running on ports ${PORT}`));
